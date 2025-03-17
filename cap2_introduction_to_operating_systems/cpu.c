fb48b9098d7f6aa23809bfd8422c56a5f1c55e40
Brum <gabrielbrum2805@gmail.com>
2025-03-17 10:17:08 -0300

    adding how to run and tips about it

--- cap2_introduction_to_operating_systems/cpu.c
+++ cap2_introduction_to_operating_systems/cpu.c
@@ -1,10 +1,19 @@
 /*
 
 fig 2.1
-    - we can run this program some times simultaneosly 
-    - this is the virtualization, where a unique cpu can run more than one programa at the same time
-    ./cpu A & ./cpu B & ./cpu C & ./cpu D &
-
+  - we can run this program some times simultaneosly 
+  - this is the virtualization, where a unique cpu can run more than one programa at the same time
+  
+  run like this: ./cpu A & ./cpu B & ./cpu C & ./cpu D &
+  
+  to kill use:
+    prompt> jobs
+    [1] + Running ./cpu A &
+    [2] - Running ./cpu B &
+    [3]   Running ./cpu C &
+    [4]   Running ./cpu D &
+    prompt> kill %1
+    [1] + Terminated ./cpu A 
 
 */
 #include <stdio.h>
--- cap2_introduction_to_operating_systems/mem.c
+++ cap2_introduction_to_operating_systems/mem.c
@@ -11,25 +11,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "common.h"
+#include <stdint.h>
 
 int
 main (int argc, char *argv[]) 
 {
-  if (argc != 2) {
-    fprintf(stderr, "usage: mem <value>\n");
-    exit(1);
-  }
-  int *p = malloc(sizeof(int));
+  // if (argc != 2) {
+  //   fprintf(stderr, "usage: mem <value>\n");
+  //   exit(1);
+  // }
+  int *p = malloc(sizeof(int)); //a1: malloc is a system call that allocates memory
   assert(p != NULL);
-  printf("(%d) addr pointed to by p: %p\n", (int) getpid(), p);
-  *p = atoi(argv[1]); // assign value to addr stored in p
-  *p = 0;
+  printf("(%d) address of p: %08x\n", (int) getpid(), (unsigned int) (uintptr_t) p);  //a2: getpid() returns the process id & prints the address's memory of p
+  *p = 0; //a3: puts zero into the first slot of p
 
   while (1) {
     Spin(1);
     *p = *p + 1;
-    printf("(%d) value of p: %d\n", getpid(), *p);
+    printf("(%d) p: %d\n", getpid(), *p); //a4: prints the value of p
   }
   return 0;
 }
-  
